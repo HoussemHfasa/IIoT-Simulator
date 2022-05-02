f6a950a4efc721630a14ae761b6d2b5af81ea38a
Paul S <paulschult@yahoo.de>
2022-05-02 19:30:41 +0200

    1. CommonInterface Projekt erstellt 2. Interfaces für jede Komponente erstellt 3. ISensorDataSimulator erster Entwurf

---
+++ programm/CommonInterfaces/CommonInterfaces/Class1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CommonInterfaces
+{
+    public interface IDatamanager
+    {
+
+
+    }
+
+    public interface IBrokerComm
+    {
+
+
+    }
+
+    public interface IDatastorage
+    {
+
+
+    }
+
+    public interface ISensorDataSimulator
+
+    {
+        // Alle Klassen, die diese Schnittstelle implementieren, besitzen einen Konstruktor,
+        // der alle für diesen speziellen Simulator benötigten Nutzereingaben aufnimmt,
+        // eine Liste mit Werten und eine Funktion "SimulateValues". 
+
+
+        bool SimulateValues(int Amount);
+
+
+        //würde nur für double Werte funktionieren.. wie kann man Int Werte und bools zurückgeben
+        //List<double> getdoubleValues();
+
+        //List<int> getIntValues();
+
+        //List<bool> getBoolValues();
+
+        //oder über die den Standard-getter?
+
+        //Idee: Eine große Simulatorklasse mit einzelnen Funktionen für Normalverteilung, Linear, exponential, bool..!!
+
+
+       
+    }
+}
