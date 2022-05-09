0593f8eaea9969b3b813c82c84b858c473c0886f
Paul S <paulschult@yahoo.de>
2022-05-09 14:00:07 +0200

    DummyKomponente für GUI-Testing erstellt

---
+++ programm/DummyKomponenten/DummySensorandSensorgroups/DummySensorandSensorgroups/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using CommonInterfaces;
+
+namespace DummySensorandSensorgroups
+{
+    public class Sensorgroup : ISensorGroups
+    {
+        public string Adresse { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string[] SensorIds { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public void Sensorhinzufuegen()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Sensorloeschen()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
