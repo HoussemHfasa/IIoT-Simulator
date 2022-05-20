0397f86293cec417160a4094ff036791d76d3294
LAPTOP-4LCT4HO6\49176 <memasalihi@gmail.com>
2022-05-20 17:48:41 +0200

    DummyKomponente nach Interfaceänderung angepasst

--- programm/DummyKomponenten/SensorDataSimulatorDummy/SensorDataSimulatorDummy/Class1.cs
+++ programm/DummyKomponenten/SensorDataSimulatorDummy/SensorDataSimulatorDummy/Class1.cs
@@ -7,34 +7,38 @@ namespace SensorDataSimulatorDummy
 {
     public class SensorDataSimulator : ISensorDataSimulator
     {
-        public int AmmountofValues { get { return this.AmmountofValues; } set { this.AmmountofValues = 5; } }
+        public SensorDataSimulator (uint ValueCount)
+        {
+            AmmountofValues = ValueCount;
+        }
+        public uint AmmountofValues { get { return this.AmmountofValues; } set { this.AmmountofValues = 5; } }
 
-        public List<double> GetDampedOscillation(double Amplitude, double Dampingratio, double Frequency, double Phase, int AmmountofValues)
+        public List<double> GetDampedOscillation(double Amplitude, double Dampingratio, double Frequency, double Phase)
         {
             return new List<double> { 1.1, 2.2, 3.3, 4.4, 5 };
         }
 
-        public List<double> GetExponentialValues(double Basis, double Exponent, int AmountofValues)
+        public List<double> GetExponentialValues(double Basis, double Exponent)
         {
             return new List<double> { 1.1, 2.2, 3 };
         }
 
-        public List<double> GetHarmonicOscillation(double Amplitude, double Frequency, double Phase, int AmmountofValues)
+        public List<double> GetHarmonicOscillation(double Amplitude, double Frequency, double Phase)
         {
             return new List<double> { 1.1, 2.2, 3.3, 4.4, 5 };
         }
 
-        public List<double> GetLinearValues(double Slope, double XShift, int AmmountofValues)
+        public List<double> GetLinearValues(double Slope, double XShift)
         {
             return new List<double> { 1.1, 2.2, 3.3, 4.4, 5 };
         }
 
-        public List<bool> GetRandomBoolValues(double Wechselwarscheinlichkeit, int AmountofValues)
+        public List<bool> GetRandomBoolValues(double Wechselwarscheinlichkeit)
         {
             return new List<bool> { true, false, true };
         }
 
-        public List<double> GetStandardDeviationValues(double Mean, double StandardDeviation, int AmmountofValues)
+        public List<double> GetStandardDeviationValues(double Mean, double StandardDeviation)
         {
             return new List<double> { 1.1, 2.2, 3.3, 4.4, 5 };
         }
