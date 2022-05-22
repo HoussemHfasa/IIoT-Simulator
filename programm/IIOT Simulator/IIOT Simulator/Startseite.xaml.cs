bf1f53407c5ecf59d65d993e3f3f93d5a7b8d365
LAPTOP-4LCT4HO6\49176 <memasalihi@gmail.com>
2022-05-22 14:53:26 +0200

    GUI erste Konzepte Startseite und Broker Einstellungen

---
+++ programm/IIOT Simulator/IIOT Simulator/Startseite.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace IIOT_Simulator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BrokerSettingsClick(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Hier kommt man zu den Broker Einstellungen");
+        }
+
+        private void NewSensorGroupClick(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Hier kann man eine neue Sensorgruppe erstellen");
+        }
+
+        private void PresentSensorGroupClick(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Hier kann man bestehende Sensorgruppen laden");
+        }
+
+        private void OpenWindow(object sender, RoutedEventArgs e)
+        {
+            BrokerEinstellungen objectBrokerEinstellungen = new BrokerEinstellungen();
+            this.Visibility = Visibility.Hidden; //So wird das aktuelle Fenster dann geschlossen
+            objectBrokerEinstellungen.Show();
+        }
+    }
+}
