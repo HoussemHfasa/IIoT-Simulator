5f3691978097e2185f414248a47adb7a388d242f
Houssem Hfasa <Houssem.Hfasa@Student.HTW-Berlin.de>
2022-05-24 17:12:10 +0200

    DataStorageDummy nach Interfaceänderung angepasst

--- programm/DummyKomponenten/DataStorageDummy/DataStorageDummy/Class1.cs
+++ programm/DummyKomponenten/DataStorageDummy/DataStorageDummy/Class1.cs
@@ -1,25 +1,34 @@
 ﻿using System;
+using System.Collections.Generic;
 using CommonInterfaces;
 
 namespace DataStorageDummy
 {
-    public class DataStorage : IDatastorage
+    public class DataStorage : IDatastorage<double> 
     {
-        public object Data { get { return this.Data; } set { this.Data = 5; } }
-        public string filepath { get { return this.filepath} set { this.filepath = "funktioniert"; } }
+        
+        public string filepath { get { return this.filepath; } set { this.filepath = "funktioniert"; } }
 
-        public object JsonDeserialize(string filepath)
+        Dictionary<DateTime, List<double>> Data { get { return Data; } set { Data.Add(DateTime.Today, new List<double> { 251, 14, 25, 48, 2.41 }); } }
+
+        Dictionary<DateTime, List<double>> IDatastorage<double>.Data { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        Dictionary<DateTime, List<double>> IDatastorage<double>.JsonDeserialize(string filepath)
         {
-            object beispiel = 5;
+            Dictionary<DateTime, List<double>> beispiel = new Dictionary<DateTime, List<double>> { };
+            beispiel.Add(DateTime.Today, new List<double> { 251, 14, 25, 48, 2.41 });
+           
             return beispiel;
         }
 
-        public void JsonSerialize(object data, string filepath)
+        public void JsonSerialize(Dictionary<DateTime, List<double>> data, string filepath)
         {
-            data = 5;
+            data.Add(DateTime.Today,new List<double> {251,14,25,48,2.41 });
             filepath = "funktioniert";
-            Console.WriteLine((string)data, filepath);
+            Console.WriteLine(data.ToString(), filepath);
         }
+
+       
 
         public object LoadBrockerProfile(string filepath)
         {
@@ -33,6 +42,12 @@ namespace DataStorageDummy
             return beispiel;
         }
 
+        public List<string> LoadSensorgroup(string Base, string Node)
+        {
+            List<string> beispiel = new List<string> { "fdv51654","dfbfdbfd651","f+ä+#214" };
+            return beispiel;
+        }
+
         public void SavebrockerProfile(object data, string filepath)
         {
             data = 5;
@@ -40,11 +55,18 @@ namespace DataStorageDummy
             Console.WriteLine((string)data, filepath);
         }
 
-        public void SaveSensorgroup(object data, string filepath)
+       
+
+        public void SaveSensorgroup(List<string> SensorListe, string Base, string Node)
         {
-            data = 5;
-            filepath = "funktioniert";
-            Console.WriteLine((string)data, filepath);
+            SensorListe= new List<string> { "fdv51654","dfbfdbfd651","f+ä+#214"} ;
+            Base = "test1";
+            Node = "test2";
+            Console.WriteLine(SensorListe.ToString(), filepath);
         }
+
+       
+
+       
     }
 }
