1a1e42fe83d3d2db8f297656a0ca1c0f8fa41cf3
LAPTOP-4LCT4HO6\49176 <memasalihi@gmail.com>
2022-06-05 12:09:59 +0200

    GUI weiter bearbeitet

---
+++ programm/IIOT Simulator/IIOT Simulator/Dataiexplorer.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace IIOT_Simulator
+{
+    /// <summary>
+    /// Interaktionslogik für Dataiexplorer.xaml
+    /// </summary>
+    public partial class Dataiexplorer : Window
+    {
+        public Dataiexplorer()
+        {
+            InitializeComponent();
+        }
+
+        private void AbbrechenButton(object sender, RoutedEventArgs e)
+        {
+            Startseite objectStartseite3 = new Startseite();
+            this.Visibility = Visibility.Hidden; //So wird das aktuelle Fenster dann geschlossen
+            objectStartseite3.Show();
+        }
+    }
+}
