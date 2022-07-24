07471d3958212c30bcf7f2b93b1466b67e453c25
Paul S <paulschult@yahoo.de>
2022-07-24 16:52:56 +0200

    Neue Sensorgruppe UI Seite gibt bei falschen Eingaben Popup Fenster aus

--- programm/IIOT Simulator/IIoTSimulatorUI/NeueSensorgruppeUI.xaml.cs
+++ programm/IIOT Simulator/IIoTSimulatorUI/NeueSensorgruppeUI.xaml.cs
@@ -56,47 +56,76 @@ namespace IIoTSimulatorUI
         //Button um den Stamm hinzuzufügen
         private void StammHinzufuegen(object sender, RoutedEventArgs e)
         {
-            string stammText = textBoxEingabe.Text; //Benutzer Eingabe in einem string speichern
+            if ((textBoxEingabe.Text).Equals(""))
+            {
+                // Kein Name eingetragen
+                MessageBox.Show("Tragen Sie einen Namen für den Stamm ein");
+            }
+            else
+            {
+                string stammText = textBoxEingabe.Text; //Benutzer Eingabe in einem string speichern
 
-            TreeViewItem stamm = new TreeViewItem(); //Neues Treeviewtem für den Stamm erstellen
+                TreeViewItem stamm = new TreeViewItem(); //Neues Treeviewtem für den Stamm erstellen
 
-            stamm.Header = stammText; //Dem TreeViewItem den Header übergeben
+                stamm.Header = stammText; //Dem TreeViewItem den Header übergeben
 
-            TreeView1.Items.Add(stamm); //Der TreeView den Stamm hinzufügen
+                TreeView1.Items.Add(stamm); //Der TreeView den Stamm hinzufügen
 
-            textBoxEingabe.Clear(); //TextBox Eingabe wieder löschen
+                textBoxEingabe.Clear(); //TextBox Eingabe wieder löschen
 
-            HakenStamm.Foreground = System.Windows.Media.Brushes.Green; //Grünen Haken erscheinen lassen
+                HakenStamm.Foreground = System.Windows.Media.Brushes.Green; //Grünen Haken erscheinen lassen
 
-            LabelInfo.Foreground = System.Windows.Media.Brushes.White; //Info ausblenden
+                LabelInfo.Foreground = System.Windows.Media.Brushes.White; //Info ausblenden
 
-            //Sensorgroups sensorgroupsObject = new Sensorgroups();
+                //Sensorgroups sensorgroupsObject = new Sensorgroups();
 
-            //sensorgroupsObject.AddBase(stammText);
+                //sensorgroupsObject.AddBase(stammText);
 
-            //
-            Sensorgroup.Add_new_Base(stammText);
+                //
+                Sensorgroup.Add_new_Base(stammText);
+            }
 
         }
 
         //Button um den Unterordner hinzuzufügen
         private void UnterordnerHinzufuegenClick(object sender, RoutedEventArgs e)
         {
-            TreeViewItem selectedTVI = (TreeViewItem)TreeView1.SelectedItem as TreeViewItem; //Ein TreeViewItem vom ausgewählten Item erstellen
-           
-            string oberordner = selectedTVI.Header.ToString();//Das ausgewählte Objekt in String speichern
+            //Falls keine Name eingetragen wurde MessageBox aufrufen
+            if ((textBoxEingabe2.Text).Equals(""))
+            {
+                // Kein Name eingetragen
+                MessageBox.Show("Tragen Sie einen Namen für den Unterordner ein");
+            }
+            else
+            {
 
-            TreeViewItem unterordner = new TreeViewItem(); //Ein TreeViewItem vom Unterordner erstellen
+                // try catch für NullReferenceException (Kein Ordner ausgewählt
+                try
+                {
+                    TreeViewItem selectedTVI = (TreeViewItem)TreeView1.SelectedItem as TreeViewItem; //Ein TreeViewItem vom ausgewählten Item erstellen
 
-            string unterordnerText = textBoxEingabe2.Text; //Benutzereingabe in einem string speichern
 
-            unterordner.Header = unterordnerText;
+                    string oberordner = selectedTVI.Header.ToString();//Das ausgewählte Objekt in String speichern
 
-            selectedTVI.Items.Add(unterordner);// Dem ausgewählten Item den Unterordner hinzufügen
+                    TreeViewItem unterordner = new TreeViewItem(); //Ein TreeViewItem vom Unterordner erstellen
 
-            //sensorgroupsObject.AddNode(unterordnerText, oberordner);
+                    string unterordnerText = textBoxEingabe2.Text; //Benutzereingabe in einem string speichern
 
-            Sensorgroup.Add_new_Node(oberordner, unterordnerText);
+                    unterordner.Header = unterordnerText;
+
+                    selectedTVI.Items.Add(unterordner);// Dem ausgewählten Item den Unterordner hinzufügen
+
+                    //sensorgroupsObject.AddNode(unterordnerText, oberordner);
+
+                    Sensorgroup.Add_new_Node(oberordner, unterordnerText);
+                }
+                catch (System.NullReferenceException)
+                {
+                    MessageBox.Show("Wählen Sie zuerst einen übergeordnet Ordner für den neuen Ordner aus.");
+
+                }
+            }
+
         }
 
 
@@ -123,105 +152,141 @@ namespace IIoTSimulatorUI
         //Löscht die ganze Sensorgruppe
         private void SensorgruppeLoeschen(object sender, RoutedEventArgs e)
         {
-            DoubleSensor.GetValues();
+            
             TreeView1.Items.Clear();
         }
 
         //Button um zu den Sensordaten-Einstellungen zu gelangen
         private void Sensordaten(object sender, RoutedEventArgs e)
         {
-            // TODO: Für diesen Button muss auch die Sensortypbox ausgewählt sein. Dann kann hier bereits parallel zum Treeview 
-            // unser Sensor erzeugt und zur Sensorgroup hinzugefügt werden
-
-
-            TreeViewItem selectedTVI = (TreeViewItem)TreeView1.SelectedItem as TreeViewItem; //Ein TreeViewItem vom ausgewählten Item erstellen
-           
-            
-            string oberordner = selectedTVI.Header.ToString();//Das ausgewählte Objekt in String speichern
-
-            TreeViewItem sensorname = new TreeViewItem(); //Ein TreeViewItem vom Sensornamen erstellen
-
             string textSensorname = textBoxEingabeSensor.Text; //Benutzereingabe in einem string speichern
-
-            sensorname.Header = textSensorname;
-
-            selectedTVI.Items.Add(sensorname);
-
-            // Auswahl für die verschiedenen Sensoren, die der Nutzer genommen hat
-            // und Hinzufügen des Sensors in unsere Sensorgroup
             string SensortypAuswahl = SensortypBox.Text;
-            if (SensortypAuswahl == "Temperatursensor")
+
+            //Nutzer hat keinen Namen eingegeben
+            if (textSensorname.Equals(""))
             {
-                DoubleSensor = new TemperatureSensor();
-                DoubleSensor.Sensor_id = textSensorname;
-                Sensorgroup.Add_new_Sensor(oberordner, DoubleSensor);
+                MessageBox.Show("Geben Sie einen Namen für den Sensor ein");
             }
-            else if (SensortypAuswahl == "Helligkeitssensor")
-            {
-                DoubleSensor = new BrightnessSensor();
-                DoubleSensor.Sensor_id = textSensorname;
-                Sensorgroup.Add_new_Sensor(oberordner, DoubleSensor);
-            }
-            else if (SensortypAuswahl == "Stromsensor")
-            {
-                DoubleSensor = new CurrentSensor();
-                DoubleSensor.Sensor_id = textSensorname;
-                Sensorgroup.Add_new_Sensor(oberordner, DoubleSensor);
-            }
-            else if (SensortypAuswahl == "Rauchmelder")
-            {
-                BoolSensor = new firedetector();
-                BoolSensor.Sensor_id = textSensorname;
-                Sensorgroup.Add_new_Sensor(oberordner, BoolSensor);
-            }
-            else if (SensortypAuswahl == "Feuchtigkeitssensor")
-            {
-                DoubleSensor = new HumiditySensor();
-                DoubleSensor.Sensor_id = textSensorname;
-                Sensorgroup.Add_new_Sensor(oberordner, DoubleSensor);
-            }
-            else if (SensortypAuswahl == "Füllstandsensor")
-            {
-                DoubleSensor = new LevelSensor();
-                DoubleSensor.Sensor_id = textSensorname;
-                Sensorgroup.Add_new_Sensor(oberordner, DoubleSensor);
-            }
-            else if (SensortypAuswahl == "Dehnungssensor")
-            {
-                DoubleSensor = new StrainSensor();
-                DoubleSensor.Sensor_id = textSensorname;
-                Sensorgroup.Add_new_Sensor(oberordner, DoubleSensor);
-            }
-            else if (SensortypAuswahl == "Drehmomentsensor")
-            {
-                DoubleSensor = new TorqueSensor();
-                DoubleSensor.Sensor_id = textSensorname;
-                Sensorgroup.Add_new_Sensor(oberordner, DoubleSensor);
-            }
-            else if (SensortypAuswahl == "Spannungssensor")
-            {
-                DoubleSensor = new VoltageSensor();
-                DoubleSensor.Sensor_id = textSensorname;
-                Sensorgroup.Add_new_Sensor(oberordner, DoubleSensor);
-            }
-            else
+            else if(SensortypAuswahl.Equals(""))
             {
                 MessageBox.Show("Wählen Sie einen Sensortyp aus.");
             }
-
-            PopUpSensoren objectPopupSensoren;
-            
-            if (SensortypAuswahl == "Rauchmelder")
-            {
-                //Bool
-                objectPopupSensoren = new PopUpSensoren(ref BoolSensor);
-            }
+            //Nutzer hat Namen und einen Sensortypen ausgewählt eingegeben
             else
             {
-                objectPopupSensoren = new PopUpSensoren(ref DoubleSensor);
-            }
-            objectPopupSensoren.Show();
 
+
+
+                TreeViewItem selectedTVI = (TreeViewItem)TreeView1.SelectedItem as TreeViewItem; //Ein TreeViewItem vom ausgewählten Item erstellen
+
+
+                string oberordner = selectedTVI.Header.ToString();//Das ausgewählte Objekt in String speichern
+
+                TreeViewItem sensorname = new TreeViewItem(); //Ein TreeViewItem vom Sensornamen erstellen            
+
+                sensorname.Header = textSensorname;
+
+                selectedTVI.Items.Add(sensorname);
+
+                // Auswahl für die verschiedenen Sensoren, die der Nutzer genommen hat
+                // und Hinzufügen des Sensors in unsere Sensorgroup
+                
+
+                // Funktioniert noch nicht wie gewollt
+
+                if (SensortypAuswahl == "Temperatursensor")
+                {
+                    DoubleSensor = new TemperatureSensor();
+                    DoubleSensor.Sensor_id = textSensorname;
+                    Sensorgroup.Add_new_Sensor(oberordner, DoubleSensor);
+
+                    PopUpSensoren objectPopupSensoren;
+                    objectPopupSensoren = new PopUpSensoren(ref DoubleSensor);
+                    objectPopupSensoren.Show();
+                }
+                else if (SensortypAuswahl == "Helligkeitssensor")
+                {
+                    DoubleSensor = new BrightnessSensor();
+                    DoubleSensor.Sensor_id = textSensorname;
+                    Sensorgroup.Add_new_Sensor(oberordner, DoubleSensor);
+
+                    PopUpSensoren objectPopupSensoren;
+                    objectPopupSensoren = new PopUpSensoren(ref DoubleSensor);
+                    objectPopupSensoren.Show();
+                }
+                else if (SensortypAuswahl == "Stromsensor")
+                {
+                    DoubleSensor = new CurrentSensor();
+                    DoubleSensor.Sensor_id = textSensorname;
+                    Sensorgroup.Add_new_Sensor(oberordner, DoubleSensor);
+
+                    PopUpSensoren objectPopupSensoren;
+                    objectPopupSensoren = new PopUpSensoren(ref DoubleSensor);
+                    objectPopupSensoren.Show();
+                }
+                else if (SensortypAuswahl == "Rauchmelder")
+                {
+                    BoolSensor = new firedetector();
+                    BoolSensor.Sensor_id = textSensorname;
+                    Sensorgroup.Add_new_Sensor(oberordner, BoolSensor);
+
+                    ZufallsBool ObjectZufallsBool;
+                    ObjectZufallsBool = new ZufallsBool(ref BoolSensor);
+                }
+                else if (SensortypAuswahl == "Feuchtigkeitssensor")
+                {
+                    DoubleSensor = new HumiditySensor();
+                    DoubleSensor.Sensor_id = textSensorname;
+                    Sensorgroup.Add_new_Sensor(oberordner, DoubleSensor);
+
+                    PopUpSensoren objectPopupSensoren;
+                    objectPopupSensoren = new PopUpSensoren(ref DoubleSensor);
+                    objectPopupSensoren.Show();
+                }
+                else if (SensortypAuswahl == "Füllstandsensor")
+                {
+                    DoubleSensor = new LevelSensor();
+                    DoubleSensor.Sensor_id = textSensorname;
+                    Sensorgroup.Add_new_Sensor(oberordner, DoubleSensor);
+
+                    PopUpSensoren objectPopupSensoren;
+                    objectPopupSensoren = new PopUpSensoren(ref DoubleSensor);
+                    objectPopupSensoren.Show();
+                }
+                else if (SensortypAuswahl == "Dehnungssensor")
+                {
+                    DoubleSensor = new StrainSensor();
+                    DoubleSensor.Sensor_id = textSensorname;
+                    Sensorgroup.Add_new_Sensor(oberordner, DoubleSensor);
+
+                    PopUpSensoren objectPopupSensoren;
+                    objectPopupSensoren = new PopUpSensoren(ref DoubleSensor);
+                    objectPopupSensoren.Show();
+                }
+                else if (SensortypAuswahl == "Drehmomentsensor")
+                {
+                    DoubleSensor = new TorqueSensor();
+                    DoubleSensor.Sensor_id = textSensorname;
+                    Sensorgroup.Add_new_Sensor(oberordner, DoubleSensor);
+
+                    PopUpSensoren objectPopupSensoren;
+                    objectPopupSensoren = new PopUpSensoren(ref DoubleSensor);
+                    objectPopupSensoren.Show();
+                }
+                else if (SensortypAuswahl == "Spannungssensor")
+                {
+                    DoubleSensor = new VoltageSensor();
+                    DoubleSensor.Sensor_id = textSensorname;
+                    Sensorgroup.Add_new_Sensor(oberordner, DoubleSensor);
+
+                    PopUpSensoren objectPopupSensoren;
+                    objectPopupSensoren = new PopUpSensoren(ref DoubleSensor);
+                    objectPopupSensoren.Show();
+                }
+
+
+
+            }
 
         }
     }
