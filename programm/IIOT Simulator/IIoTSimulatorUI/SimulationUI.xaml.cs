8963d84cae01699cd1b5aa555fa088c3c93fb0f7
Mjellma Salihi <memasalihi@gmail.com>
2022-07-25 21:31:15 +0200

    no message

--- programm/IIOT Simulator/IIoTSimulatorUI/SimulationUI.xaml.cs
+++ programm/IIOT Simulator/IIoTSimulatorUI/SimulationUI.xaml.cs
@@ -15,7 +15,7 @@ using LiveCharts.Wpf;
 using DummySensorandSensorgroups;
 using SensorAndSensorgroup;
 using SensorDataSimulator;
-
+using System.Threading.Tasks;
 
 namespace IIoTSimulatorUI
 {
@@ -25,11 +25,239 @@ namespace IIoTSimulatorUI
     public partial class SimulationUI : Window 
     {
         Sensorgroups Sensorgroup;
-        
+
+        private Line xAxisLine, yAxisLine;
+        private double xAxisStart = 30, yAxisStart = 30, interval = 40;
+        private Polyline chartPolyline;
+
+        private Point origin;
+        private List<Holder> holders;
+        private List<Value> values;
+
         public SimulationUI(Sensorgroups ExistingSensorgroup)
         {
             this.Sensorgroup = ExistingSensorgroup;
             InitializeComponent();
+
+
+            holders = new List<Holder>();
+            values = new List<Value>()
+            {
+                //new Value(0,0),
+                //new Value(100,100),
+                //new Value(200,200),
+                //new Value(300,300),
+                //new Value(400,200),
+                //new Value(500,500),
+                //new Value(600,500),
+                //new Value(700,500),
+                //new Value(800,500),
+                //new Value(900,600),
+                //new Value(1000,200),
+                //new Value(1100,100),
+                //new Value(1200,400),
+
+                //new Value(0,0),
+                //new Value(100,200),
+                //new Value(200,100),
+                //new Value(300,200),
+                //new Value(400,300),
+                //new Value(500,400),
+                //new Value(600,500),
+                //new Value(700,400),
+                //new Value(800,500),
+                //new Value(900,600),
+                //new Value(1000,300),
+                //new Value(1100,100),
+                //new Value(1200,400),
+
+                new Value(0,0),
+                new Value(100,100),
+                new Value(200,400),
+                new Value(300,200),
+                new Value(400,400),
+                new Value(500,300),
+                new Value(600,100),
+                new Value(700,700),
+                new Value(800,200),
+                new Value(900,600),
+                new Value(1000,600),
+                new Value(1100,0),
+                new Value(1200,100),
+                new Value(1300,100),
+            };
+
+            Paint();
+
+            this.StateChanged += (sender, e) => Paint();
+            this.SizeChanged += (sender, e) => Paint();
+        }
+
+        public void Paint()
+        {
+            try
+            {
+                if (this.ActualWidth > 0 && this.ActualHeight > 0)
+                {
+                    chartCanvas.Children.Clear();
+                    holders.Clear();
+
+                    // axis lines
+                    xAxisLine = new Line()
+                    {
+                        X1 = xAxisStart ,
+                        Y1 = this.ActualHeight - yAxisStart,
+                        X2 = this.ActualWidth - xAxisStart,
+                        Y2 = this.ActualHeight - yAxisStart,
+                        Stroke = Brushes.LightGray,
+                        StrokeThickness = 0.5,
+                    };
+                    yAxisLine = new Line()
+                    {
+                        X1 = xAxisStart,
+                        Y1 = yAxisStart ,
+                        X2 = xAxisStart,
+                        Y2 = this.ActualHeight - yAxisStart,
+                        Stroke = Brushes.LightGray,
+                        StrokeThickness = 0.5,
+                    };
+
+                    chartCanvas.Children.Add(xAxisLine);
+                    chartCanvas.Children.Add(yAxisLine);
+
+                    origin = new Point(xAxisLine.X1, yAxisLine.Y2);
+
+                    var xTextBlock0 = new TextBlock() { Text = $"{0}" };
+                    chartCanvas.Children.Add(xTextBlock0);
+                    Canvas.SetLeft(xTextBlock0, origin.X);
+                    Canvas.SetTop(xTextBlock0, origin.Y + 5);
+
+                    // y axis lines
+                    var xValue = xAxisStart;
+                    double xPoint = origin.X + interval;
+                    while (xPoint < xAxisLine.X2)
+                    {
+                        var line = new Line()
+                        {
+                            X1 = xPoint,
+                            Y1 = yAxisStart,
+                            X2 = xPoint,
+                            Y2 = this.ActualHeight - yAxisStart,
+                            Stroke = Brushes.LightGray,
+                            StrokeThickness = 2,
+                            Opacity = 1,
+                        };
+
+                        chartCanvas.Children.Add(line);
+
+                        var textBlock = new TextBlock { Text = $"{xValue}", };
+
+                        chartCanvas.Children.Add(textBlock);
+                        Canvas.SetLeft(textBlock, xPoint - 12.5);
+                        Canvas.SetTop(textBlock, line.Y2 + 5);
+
+                        xPoint += interval;
+                        xValue += interval;
+                    }
+
+
+                    var yTextBlock0 = new TextBlock() { Text = $"{0}" };
+                    chartCanvas.Children.Add(yTextBlock0);
+                    Canvas.SetLeft(yTextBlock0, origin.X - 20);
+                    Canvas.SetTop(yTextBlock0, origin.Y - 10);
+
+                    // x axis lines
+                    var yValue = yAxisStart;
+                    double yPoint = origin.Y - interval;
+                    while (yPoint > yAxisLine.Y1)
+                    {
+                        var line = new Line()
+                        {
+                            X1 = xAxisStart,
+                            Y1 = yPoint,
+                            X2 = this.ActualWidth - xAxisStart,
+                            Y2 = yPoint,
+                            Stroke = Brushes.LightGray,
+                            StrokeThickness = 2,
+                            Opacity = 0,
+                        };
+
+                        chartCanvas.Children.Add(line);
+
+                        var textBlock = new TextBlock() { Text = $"{yValue}" };
+                        chartCanvas.Children.Add(textBlock);
+                        Canvas.SetLeft(textBlock, line.X1 - 30);
+                        Canvas.SetTop(textBlock, yPoint - 10);
+
+                        yPoint -= interval;
+                        yValue += interval;
+                    }
+
+                    // connections
+                    double x = 0, y = 0;
+                    xPoint = origin.X;
+                    yPoint = origin.Y;
+                    while (xPoint < xAxisLine.X2)
+                    {
+                        while (yPoint > yAxisLine.Y1)
+                        {
+                            var holder = new Holder()
+                            {
+                                X = x,
+                                Y = y,
+                                Point = new Point(xPoint, yPoint),
+                            };
+
+                            holders.Add(holder);
+
+                            yPoint -= interval;
+                            y += interval;
+                        }
+
+                        xPoint += interval;
+                        yPoint = origin.Y;
+                        x += 100;
+                        y = 0;
+                    }
+
+                    // polyline
+                    chartPolyline = new Polyline()
+                    {
+                        Stroke = new SolidColorBrush(Color.FromRgb(68, 114, 196)),
+                        StrokeThickness = 10,
+                    };
+                    chartCanvas.Children.Add(chartPolyline);
+
+                    // showing where are the connections points
+                    foreach (var holder in holders)
+                    {
+                        Ellipse oEllipse = new Ellipse()
+                        {
+                            Fill = Brushes.Red,
+                            Width = 10,
+                            Height = 10,
+                            Opacity = 0,
+                        };
+
+                        chartCanvas.Children.Add(oEllipse);
+                        Canvas.SetLeft(oEllipse, holder.Point.X - 5);
+                        Canvas.SetTop(oEllipse, holder.Point.Y - 5);
+                    }
+
+                    // add connection points to polyline
+                    foreach (var value in values)
+                    {
+                        var holder = holders.FirstOrDefault(h => h.X == value.X && h.Y == value.Y);
+                        if (holder != null)
+                            chartPolyline.Points.Add(holder.Point);
+                    }
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
 
             //
             /*
@@ -48,58 +276,13 @@ namespace IIoTSimulatorUI
             //Die geladene Sensorgreupe
             Sensorbeispiel sensor = new Sensorbeispiel();
 
-             LabelTopicSimulation.Text = sensor.Topic;
+            LabelTopicSimulation.Text = sensor.Topic;
 
             //Label für das Topic in der Leiste
-             LabelTopic.Content = sensor.Topic;
+            LabelTopic.Content = sensor.Topic;
 
 
-            //Visualisierung der Sensordaten, bisher noch kein Binding
-            SeriesCollection = new SeriesCollection
-            {
-                new LineSeries
-                {
-                    Title = "Series 1",
-                    Values = new ChartValues<double> { 4, 6, 5, 2 ,4 }
-                },
-                new LineSeries
-                {
-                    Title = "Series 2",
-                    Values = new ChartValues<double> { 6, 7, 3, 4 ,6 },
-                    PointGeometry = null
-                },
-                new LineSeries
-                {
-                    Title = "Series 3",
-                    Values = new ChartValues<double> { 4,2,7,2,7 },
-                    PointGeometry = DefaultGeometries.Square,
-                    PointGeometrySize = 15
-                }
-            };
-
-            Labels = new[] { "Jan", "Feb", "Mar", "Apr", "May" };
-            YFormatter = value => value.ToString("C");
-
-            SeriesCollection.Add(new LineSeries
-            {
-                Title = "Series 4",
-                Values = new ChartValues<double> { 5, 3, 2, 4 },
-                
-                LineSmoothness = 0,
-                PointGeometry = Geometry.Parse("m 25 70.36218 20 -28 -20 22 -8 -6 z"),
-                PointGeometrySize = 50,
-                PointForeground = Brushes.Gray
-            });
-
-            SeriesCollection[3].Values.Add(5d);
-
-            DataContext = this;
         }
-
-        public SeriesCollection SeriesCollection { get; set; }
-        public string[] Labels { get; set; }
-        public Func<double, string> YFormatter { get; set; }
-    
     
         private void ProgrammSchließenClick(object sender, RoutedEventArgs e)
         {
@@ -135,6 +318,33 @@ namespace IIoTSimulatorUI
         private void CartesianChart_Loaded(object sender, RoutedEventArgs e)
         {
 
+        }
+
+        private void Sensorauswaehlen(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+    public class Holder
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+        public Point Point { get; set; }
+
+        public Holder()
+        {
+        }
+    }
+
+    public class Value
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+
+        public Value(double x, double y)
+        {
+            X = x;
+            Y = y;
         }
     }
 }
