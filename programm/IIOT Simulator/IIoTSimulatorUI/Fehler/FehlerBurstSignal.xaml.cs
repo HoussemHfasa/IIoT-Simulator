9fb96126f992067e9ca40f2ca23e690a2de543d8
Houssem Hfasa <Houssem.Hfasa@Student.HTW-Berlin.de>
2022-07-27 15:06:11 +0200

    Diagrammen für FehlerErzeugungsmethoden implimentiert

--- programm/IIOT Simulator/IIoTSimulatorUI/Fehler/FehlerBurstSignal.xaml.cs
+++ programm/IIOT Simulator/IIoTSimulatorUI/Fehler/FehlerBurstSignal.xaml.cs
@@ -9,6 +9,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using LiveCharts;
+using LiveCharts.Wpf;
 using SensorAndSensorgroup;
 using SensorDataSimulator;
 
@@ -22,32 +24,83 @@ namespace IIoTSimulatorUI
         Sensor<double> DoubleSensor;
         public FehlerBurstSignal(ref Sensor<double> NewSensor)
         {
+            Boolconstructor = true;
             this.DoubleSensor = NewSensor;
             InitializeComponent();
         }
+        List<double> Datenliste;
+        bool Boolconstructor;
+        // Für neue Linechart
 
+        public SeriesCollection SeriesCollection { get; set; }
+        public string[] Labels { get; set; }
+        public Func<double, string> YFormatter { get; set; }
+        private ChartValues<double> values;
+
+        public FehlerBurstSignal(ref SensorAndSensorgroup.Sensor<double> NewSensor, List<double> Sensordaten)
+        {
+            Boolconstructor = false;
+            this.DoubleSensor = NewSensor;
+            this.Datenliste = Sensordaten;
+            values = new ChartValues<double>();
+            int[] Labelsint = null;
+            InitializeComponent();
+            SeriesCollection = new SeriesCollection
+            {
+            };
+            Labelsint = new int[Datenliste.Count];
+            Labels = new string[Datenliste.Count];
+            for (int i = 0; i < Datenliste.Count; i++)
+            {
+                Labelsint[i] = i;
+            }
+            if (!(Labelsint == null))
+            {
+                Labels = Array.ConvertAll(Labelsint, x => x.ToString());
+            }
+            values = new ChartValues<double>();
+            values.AddRange(Datenliste);
+
+            // Beschriftung der Werte YAchse
+            YFormatter = value => value.ToString("");
+            //modifying the series collection will animate and update the chart
+
+            SeriesCollection.Add(new LineSeries
+            {
+                Title = DoubleSensor.Sensor_id,
+                Values = values,
+                LineSmoothness = 0,
+                PointForeground = Brushes.Gray
+            });
+            DataContext = this;
+        }
         private void Hinzufügen(object sender, RoutedEventArgs e)
         {
-            
             try
-            { 
-            //Nutzereingabe ErrorRate überprüfen
-            if((Convert.ToDouble(textBoxErrorRate.Text) < 0.0) || (Convert.ToDouble(textBoxErrorRate.Text) > 1))
             {
-                MessageBox.Show("Die Fehlerrate muss zwischen 0,0 und 1,0 liegen.");
-            }
-            else if(Convert.ToInt32(textBoxBurstduration.Text) < 0)
-            {
-                MessageBox.Show("Die Fehlerdauer darf nicht kleiner als 0 sein.");
-            }
-            else
-            {
-            // Fehlerdatengenerator erzeugen, Sensordaten mit Fehlern versehen
-            BurstNoise DataGenerator = new BurstNoise(Convert.ToDouble(textBoxBurstvalue.Text), Convert.ToInt32(textBoxBurstduration.Text), Convert.ToDouble(textBoxErrorRate.Text));
-            DoubleSensor.SetValues(DataGenerator.GetSensorDataWithErrors(DoubleSensor.GetValues()));
-
-            Close();
+                if (Boolconstructor == true)
+                {
+                    //Nutzereingabe ErrorRate überprüfen
+                    if ((Convert.ToDouble(textBoxErrorRate.Text) < 0.0) || (Convert.ToDouble(textBoxErrorRate.Text) > 1))
+                    {
+                        MessageBox.Show("Die Fehlerrate muss zwischen 0,0 und 1,0 liegen.");
+                    }
+                    else if (Convert.ToInt32(textBoxBurstduration.Text) < 0)
+                    {
+                        MessageBox.Show("Die Fehlerdauer darf nicht kleiner als 0 sein.");
+                    }
+                    else
+                    {
+                        // Fehlerdatengenerator erzeugen, Sensordaten mit Fehlern versehen
+                        BurstNoise DataGenerator = new BurstNoise(Convert.ToDouble(textBoxBurstvalue.Text), Convert.ToInt32(textBoxBurstduration.Text), Convert.ToDouble(textBoxErrorRate.Text));
+                        DoubleSensor.SetValues(DataGenerator.GetSensorDataWithErrors(DoubleSensor.GetValues()));
+                    }
                 }
+                else
+                {
+                    DoubleSensor.SetValues(Datenliste);
+                }
+                Close();
             }
             catch (System.FormatException)
             {
@@ -63,7 +116,31 @@ namespace IIoTSimulatorUI
 
         private void Aktualisieren(object sender, RoutedEventArgs e)
         {
-
+            try
+            {
+                //Nutzereingabe ErrorRate überprüfen
+                if ((Convert.ToDouble(textBoxErrorRate.Text) < 0.0) || (Convert.ToDouble(textBoxErrorRate.Text) > 1))
+                {
+                    MessageBox.Show("Die Fehlerrate muss zwischen 0,0 und 1,0 liegen.");
+                }
+                else if (Convert.ToInt32(textBoxBurstduration.Text) < 0)
+                {
+                    MessageBox.Show("Die Fehlerdauer darf nicht kleiner als 0 sein.");
+                }
+                else
+                {
+                    // Fehlerdatengenerator erzeugen, Sensordaten mit Fehlern versehen
+                    BurstNoise DataGenerator = new BurstNoise(Convert.ToDouble(textBoxBurstvalue.Text), Convert.ToInt32(textBoxBurstduration.Text), Convert.ToDouble(textBoxErrorRate.Text));
+                    Datenliste= DataGenerator.GetSensorDataWithErrors(DoubleSensor.GetValues());
+                    FehlerBurstSignal Aktualisirung = new FehlerBurstSignal(ref DoubleSensor, Datenliste);
+                    this.Visibility = Visibility.Hidden;
+                    Aktualisirung.Show();
+                }
+            }
+            catch (System.FormatException)
+            {
+                MessageBox.Show("Ungültige Eingabe");
+            }
         }
     }
 }
